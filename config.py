f4dbbf7f60cdafbe1dcbf2937ed7a253a4d7beca
Gabriel Silva Nobrega <57645902+biellSilva@users.noreply.github.com>
2022-12-15 20:16:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,46 @@
+import dotenv
+import os
+import pytz
 
-# MONGODB 
+dotenv.load_dotenv(dotenv.find_dotenv())
+client = os.getenv('mongoClient')
+Pw = os.getenv('mongoPW')
 
-
-# COLORS
-
-
-# TIMEZONE
-
-
-# GUILD ID
-
-
-# ROLES ID'S
+#   PYTZ
+tz_brazil = pytz.timezone('America/Sao_Paulo')
 
 
-# CHANNEL ID'S
+#   MONGODB
+mongo = f"mongodb"
+
+
+#   GUILD
+
+
+#   CORES
+roxo = 0x690FC3
+vermelho = 0xff0000
+
+
+#   CARGOS AUTOMATICOS/STAFF -----
+
+
+#   CARGOS PING ----------------
+#   PING DIARIO
+
+
+#   PING SQUAD
+
+
+#   PING ALERTAS
+
+
+#   CARGOS PERSONALIZADOS -----
+
+
+#   CARGOS PADRÃO -----
+
+
+#   SALAS DE TEXTO ================
+
+
